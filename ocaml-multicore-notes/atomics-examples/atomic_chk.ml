eea7332096642c999bbef52a0044f57f7a043397
Sudha Parimala <sudharg247@gmail.com>
2020-02-03 12:42:13 +0530

    updates

--- ocaml-multicore-notes/atomics-examples/atomic_chk.ml
+++ ocaml-multicore-notes/atomics-examples/atomic_chk.ml
@@ -1,40 +1,35 @@
 open Printf
 
-let add_atom atom = Atomic.set atom ((Atomic.get atom)+1)
-;;
+let add_atom atom = Atomic.incr atom
 
 let add_ref my_ref = begin
   my_ref := !my_ref + 1
 end
-;;
+let a = Atomic.make 0
+let b = ref 0
 
-(* let add_val_atom atom = Domain.spawn(fun _ -> add_atom atom) |> ignore
-;;
+let rec spawn n =
+  if (n = 0) then []
+  else begin
+  Domain.spawn (fun _ -> for _ = 0 to (try int_of_string Sys.argv.(1) with _ -> 1000) do
+    add_atom a;
+    add_ref b;
+  done;) :: (spawn (n-1))
+  end
 
-let add_val_ref my_ref = Domain.spawn(fun _ -> add_ref my_ref) |> ignore
-;; *)
-
-let add_val_atom atom = Domain.spawn(fun _ -> add_atom atom) |> ignore;;
-
-let add_val_ref my_ref = Domain.spawn(fun _ -> add_ref my_ref) |> ignore;;
+let number_domains = try int_of_string Sys.argv.(2) with _ -> 4
 
 let _ =
-  let a = Atomic.make 0 in
-  let b = ref 0 in
   begin
-    (* Domain.spawn(fun _ -> Atomic.set a ((Atomic.get a)+5)) |> ignore;
-    Domain.spawn(fun _ -> Atomic.set a ((Atomic.get a) + 10)) |> ignore;
-    Domain.spawn(fun _ -> b := !b + 5) |> ignore;
-    Domain.spawn(fun _ -> b := !b + 10) |> Domain.join; *)
-
-    (* for i = 0 to (int_of_string Sys.argv.(1)) do
-      add_val_atom a;
-      add_val_ref b;
-    done; *)
+  let domains = spawn number_domains
+  in List.iter Domain.join domains;
+  printf "a = %d\nb = %d\n" (Atomic.get a) (!b)
+  end
+(*
+  begin
     Domain.spawn (fun _ -> for _ = 0 to (int_of_string Sys.argv.(1)) do
       add_atom a;
       add_ref b;
     done;) |> Domain.join;
-
     printf "a = %d\nb = %d\n" (Atomic.get a) (!b)
-  end
+  end *)
